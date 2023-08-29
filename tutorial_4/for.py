ba6a75f20b80cbcf350bb712567942c079f6703d
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-29 18:21:23 +0200

    Added for.py module

---
+++ tutorial_4/for.py
@@ -0,0 +1,11 @@
+# Section 4.2
+# https://docs.python.org/3.11/tutorial/controlflow.html
+
+def main():
+	words = ['cat', 'dog', 'giraffe']
+	print("word, len")
+	for w in words:
+		print(w, len(w))
+
+if __name__ == "__main__":
+	main()
