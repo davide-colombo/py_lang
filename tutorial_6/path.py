17a0ab3ee4ff362448d2c2432bb740ed1384ed96
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-29 16:36:41 +0200

    Added sys.path and os.pathsep print methods

--- tutorial_6/path.py
+++ tutorial_6/path.py
@@ -6,9 +6,20 @@
 # list.
 
 def print_builtin_module_names():
+	print("List of builtin modules:")
 	for n in sys.builtin_module_names:
 		print(n)
+	print()
+
+def print_syspath():
+	print("sys.path = ", sys.path)
+
+def print_pathsep():
+	print("os.pathsep = ", os.pathsep)
 
 if __name__ == "__main__":
 	import sys
+	import os
 	print_builtin_module_names()
+	print_pathsep()
+	print_syspath()
