1c6b475da03922638559bd15b010b41dd9c44d1b
Davide Colombo <davide.colombo02@protonmail.com>
2023-08-30 17:25:20 +0200

    Added 'tutorial_5/tuple.py'

---
+++ tutorial_5/tuple.py
@@ -0,0 +1,9 @@
+# Section 5.3
+# https://docs.python.org/3.11/tutorial/datastructures.html#tuples-and-sequences
+
+def tup():
+	t = 1234, 5678, "Hello"
+	print(t)
+
+if __name__ == '__main__':
+	tup()
