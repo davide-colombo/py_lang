c214a6a0b6ca276deac1a3fd225f335903a197fb
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-01 14:56:12 +0200

    BIOPYTHON: complement() & reverse_complement()

--- tutorial_biopy/sequence.py
+++ tutorial_biopy/sequence.py
@@ -5,10 +5,23 @@ import sys
 from Bio.Seq import Seq
 
 def main():
+
+	print(50 * '-')
+	print('Creating an object of type {type(Seq)}')
 	mysequence = Seq("AGTACACTGGT")
 	print(mysequence)
 	print(type(mysequence))
 	print(sys.getsizeof(mysequence))
 
+	print(50 * '-')
+	print(f'Get the complement of {mysequence!r}')
+	complement = mysequence.complement()
+	print(complement)
+
+	print(50 * '-')
+	print(f'Get the reverse complement of {mysequence!r}')
+	reverse = mysequence.reverse_complement()
+	print(reverse)
+	
 if __name__ == '__main__':
 	main()
