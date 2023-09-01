726d83cd7e33e38e9572217144797711a8eab0dc
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-01 15:13:08 +0200

    ISVALIDNASEQ: check whether a string is a valid DNA sequence

--- tutorial_biopy/sequence.py
+++ tutorial_biopy/sequence.py
@@ -2,20 +2,44 @@
 # http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec7
 
 import sys
+import re
 from Bio.Seq import Seq
+
+# ============================================================================
+def isvalidnaseq(dnaseq:str) -> bool:
+	if dnaseq:
+		return bool(re.match(r'^[ATGC]+$', dnaseq))
+	return False
+
+# ============================================================================
 
 COMPLEMENT_DICTIONARY = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
 def my_complement(dnaseq: str):
-	res = [COMPLEMENT_DICTIONARY[n] for n in dnaseq]
-	return "".join(res)
+	if not isvalidnaseq(dnaseq):
+		raise ValueError('"%s" is not a valid DNA sequence' % dnaseq)
+	return "".join([COMPLEMENT_DICTIONARY[n] for n in dnaseq])
 
+# ============================================================================
 def main():
 	print(50 * '-')
 	print('Plain string DNA sequence')
 	dnaseq = "AGTACACTGGT"
 	print(dnaseq)
 
+	print(50 * '-')
+	print(f'{dnaseq!r} is a valid DNA sequence?')
+	r = isvalidnaseq(dnaseq)
+	assert r == True
+	print(r)
+
+	print(50 * '-')
+	invalid_dnaseq = 'ATGXGCATTGAH'
+	print(f'{invalid_dnaseq!r} is a valid DNA sequence?')
+	r = isvalidnaseq(invalid_dnaseq)
+	assert r == False
+	print(r)
+	
 	print(50 * '-')
 	print('Creating an object of type {type(Seq)}')
 	mysequence = Seq(dnaseq)
