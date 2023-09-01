c14ce7e10c6f57fb5da4909a19060b9144d9e640
Davide Colombo <davide.colombo02@protonmail.com>
2023-09-01 15:02:12 +0200

    MY_COMPLEMENT: function to compute complement

--- tutorial_biopy/sequence.py
+++ tutorial_biopy/sequence.py
@@ -4,18 +4,31 @@
 import sys
 from Bio.Seq import Seq
 
+COMPLEMENT_DICTIONARY = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+def my_complement(dnaseq: str):
+	res = [COMPLEMENT_DICTIONARY[n] for n in dnaseq]
+	return "".join(res)
+
 def main():
+	print(50 * '-')
+	print('Plain string DNA sequence')
+	dnaseq = "AGTACACTGGT"
+	print(dnaseq)
 
 	print(50 * '-')
 	print('Creating an object of type {type(Seq)}')
-	mysequence = Seq("AGTACACTGGT")
-	print(mysequence)
-	print(type(mysequence))
-	print(sys.getsizeof(mysequence))
+	mysequence = Seq(dnaseq)
+	print(mysequence, type(mysequence), sys.getsizeof(mysequence))
 
 	print(50 * '-')
-	print(f'Get the complement of {mysequence!r}')
+	print(f'Complement of {mysequence!r} with BIOPYTHON')
 	complement = mysequence.complement()
+	print(complement)
+
+	print(50 * '-')
+	print('Complement of {dnaseq} using my_complement()')
+	complement = my_complement(dnaseq)
 	print(complement)
 
 	print(50 * '-')
